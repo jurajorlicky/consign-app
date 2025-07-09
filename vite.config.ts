63f6821baeeb5b7a830de12e74f53eec6833b4f5
juraj22 <56106259+jurajorlicky@users.noreply.github.com>
2025-07-09 22:44:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Dôležité: názov repa v base!
 export default defineConfig({
+  base: '/consign-app/', // <-- presne podľa názvu tvojho repa!
   plugins: [react()],
-})
+});
